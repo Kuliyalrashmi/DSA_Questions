4921044197eecc90f35ac65dfd859c00ceef21ea
Kuliyalrashmi <128361999+Kuliyalrashmi@users.noreply.github.com>
2023-11-06 16:41:20 +0530

    MergeIntervals

---
+++ Array/MergeIntervals.cpp
@@ -0,0 +1,26 @@
+/*Leetcode Question 56 Merge Intervals
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+and return an array of the non-overlapping intervals that cover all the intervals in the input.
+*/
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
+        sort(intervals.begin(),intervals.end());
+        ans.push_back(intervals[0]);
+        int n=intervals.size();
+        for(int i=1;i<n;i++)
+        {
+            if(ans.back()[1]>=intervals[i][0])
+            {
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            }
+            else
+            {
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
+ 
