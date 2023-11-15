9c0a193436ec2963a65682f667942963163e9d16
Kuliyalrashmi <128361999+Kuliyalrashmi@users.noreply.github.com>
2023-11-15 06:48:56 +0530

    ReverseSinglyLL

---
+++ LinkedList/ReverseSinglyLL.cpp
@@ -0,0 +1,57 @@
+/* Leetcode Question no 206 Reverse a Singly Linked List
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+*/
+
+/* Iterative Method*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode*curr=head;
+        ListNode*prev=NULL;
+        ListNode*forward=NULL;
+        while(curr!=NULL)
+        {
+            forward=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
+        }
+        return prev;
+    }
+};
+
+
+/*Recursive Method*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL||head->next==NULL)
+        {
+            return head;
+        }
+        ListNode*curr=reverseList(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return curr;
+    }
+};
