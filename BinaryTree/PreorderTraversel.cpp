a89271d76ba31378a405bade1024dcb518af8b7e
Kuliyalrashmi <128361999+Kuliyalrashmi@users.noreply.github.com>
2023-11-24 16:08:08 +0530

    PreorderTraversel

---
+++ BinaryTree/PreorderTraversel.cpp
@@ -0,0 +1,79 @@
+/* Leetcode Question No 144 Binary Tree Preorder Traversal
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+*/
+
+
+
+/*  Recursive Solution */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void preorder(TreeNode*root,vector<int>&ans)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        ans.push_back(root->val);
+        preorder(root->left,ans);
+        preorder(root->right,ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        preorder(root,ans);
+        return ans;
+    }
+};
+
+
+
+
+/* Iterative Solution */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        stack<TreeNode*>st;
+        if(root==NULL)
+        {
+            return ans;
+        }
+        st.push(root);
+        while(!st.empty())
+        {
+            root=st.top();
+            ans.push_back(root->val);
+            st.pop();
+            if(root->right!=NULL)
+            {
+                st.push(root->right);
+            }
+            if(root->left!=NULL)
+            {
+                st.push(root->left);
+            }
+        }
+        return ans;
+    }
+};
