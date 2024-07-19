75ea4711a1d217bcbf25a0783133f9d7e6ef02d2
Kuliyalrashmi <128361999+Kuliyalrashmi@users.noreply.github.com>
2024-07-19 21:09:37 +0530

    Time: 84 ms (61.88%), Space: 46.1 MB (47.88%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
-    void dfs(int node,vector<int>&visited,vector<vector<int>>&adj)
+    int find(int u,vector<int>&parent)
     {
-        visited[node]=1;
-        for(auto neigh:adj[node])
+        if(u==parent[u])
         {
-            if(!visited[neigh])
-            {
-                dfs(neigh,visited,adj);
-                visited[neigh]=1;
-            }
+            return u;
+        }
+        return parent[u]=find(parent[u],parent);
+    }
+    void unions(int u,int v,vector<int>&parent,vector<int>&rank)
+    {
+        u=find(u,parent);
+        v=find(v,parent);
+        if(rank[u]<rank[v])
+        {
+            parent[u]=v;
+            rank[v]++;
+        }
+        else if(rank[v]<rank[u])
+        {
+            parent[v]=u;
+            rank[u]++;
+        }
+        else
+        {
+            parent[v]=u;
+            rank[u]++;
         }
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
@@ -18,20 +34,35 @@ public:
         {
             return -1;
         }
-        vector<vector<int>>adj(n);
-        for(auto i:connections)
+        vector<int>parent(n);
+        for(int i=0;i<n;i++)
         {
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
+            parent[i]=i;
         }
-        vector<int>visited(n,0);
+        vector<int>rank(n,0);
+        
+        //count the edges that are available
+        int available=0;
+        for(auto it:connections)
+        {
+            int u=it[0];
+            int v=it[1];
+            if(find(u,parent)==find(v,parent))
+            {
+                available++;
+            }
+            else
+            {
+                unions(u,v,parent,rank);
+            }
+        }
+        // check the number of components
         int count=0;
         for(int i=0;i<n;i++)
         {
-            if(!visited[i])
+            if(parent[i]==i)
             {
-                 dfs(i,visited,adj);
-                 count++;
+                count++;
             }
         }
         return count-1;
