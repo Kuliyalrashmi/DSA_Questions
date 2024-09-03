15893baf48908c45e65f4bc8df4dff2d799bb3db
Kuliyalrashmi <128361999+Kuliyalrashmi@users.noreply.github.com>
2024-09-03 09:07:08 +0530

    Time: 445 ms (20.18%), Space: 176.8 MB (90.56%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -1,63 +1,51 @@
 class NumArray {
 public:
-    vector<int>arr,segment;
-    int n;
-    void build(int start,int end,int index)
-    {
-        if(start>=end)
-        {
-            segment[index]=arr[start];
-            return;
-        }
-        int mid=start+(end-start)/2;
-        build(start,mid,2*index+1);
-        build(mid+1,end,2*index+2);
-        segment[index]=segment[2*index+1]+segment[2*index+2];
-    }
+    int n,len;
+    vector<int>blocks,arr;
     NumArray(vector<int>& nums) {
         n=nums.size();
+        len=ceil(sqrt(n));
         arr=nums;
-        segment.resize(4*n);
-        build(0,n-1,0);
+        blocks.resize(len,0);
+        for(int i=0;i<n;i++)
+        {
+            blocks[i/len]+=nums[i];
+        }
     }
-    void update1(int start,int end,int index,int updated_index,int updated_val)
-    {
-        if(start==end)
-        {
-            segment[index]+=updated_val;
-            return;
-        }
-        int mid=start+(end-start)/2;
-        if(updated_index<=mid)
-        {
-            update1(start,mid,2*index+1,updated_index,updated_val);
-        }
-        else
-        {
-            update1(mid+1,end,2*index+2,updated_index,updated_val);
-        }
-        segment[index]=segment[2*index+1]+segment[2*index+2];
-    }
+    
     void update(int index, int val) {
         int change=val-arr[index];
         arr[index]=val;
-        update1(0,n-1,0,index,change);
+        int bi=index/len;
+        blocks[bi]+=change;
     }
-    int getsum(int start,int end,int query_start,int query_end,int index)
-    {
-        if(query_start<=start&&query_end>=end)
-        {
-            return segment[index];
-        }
-        if(query_start>end||query_end<start)
-        {
-            return 0;
-        }
-        int mid=start+(end-start)/2;
-        return getsum(start,mid,query_start,query_end,2*index+1)+getsum(mid+1,end,query_start,query_end,2*index+2);
-    }
+    
     int sumRange(int left, int right) {
-        return getsum(0,n-1,left,right,0);
+        int ans=0;
+        int sb=left/len,eb=right/len;
+        if(sb==eb)
+        {
+            for(int i=left;i<=right;i++)
+            {
+                ans+=arr[i];
+            }
+            return ans;
+        }
+        int i=left;
+        while(i/len==sb)
+        {
+            ans+=arr[i++];
+        }
+        i=right;
+        while(i/len==eb)
+        {
+            ans+=arr[i--];
+        }
+        for(i=sb+1;i<eb;i++)
+        {
+            ans+=blocks[i];
+        }
+        return ans;
     }
 };
 
