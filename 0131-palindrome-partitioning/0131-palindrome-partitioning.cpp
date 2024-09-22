12dff2ae028a2fa45f00f08e4d7079fa9390af7d
Kuliyalrashmi <128361999+Kuliyalrashmi@users.noreply.github.com>
2024-09-22 22:08:56 +0530

    Time: 157 ms (24.34%), Space: 140.3 MB (25.06%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
-    bool checkPalindrome(string &s,int start,int end)
-    {
-        while(start<=end)
-        {
-            if(s[start]!=s[end])
-            {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
-    }
-    void solve(string &s,vector<string>temp,vector<vector<string>>&ans,int index)
+     void solve(string &s,vector<string>temp,vector<vector<string>>&ans,int index, vector<vector<bool>>&dp)
     {
         if(index==s.size())
         {
@@ -22,18 +9,43 @@ public:
         }
         for(int i=index;i<s.size();i++)
         {
-            if(checkPalindrome(s,index,i))
+            if(dp[index][i])
             {
                 temp.push_back(s.substr(index,i-index+1));
-                solve(s,temp,ans,i+1);
+                solve(s,temp,ans,i+1,dp);
                 temp.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>>ans;
-        vector<string>temp;
-        solve(s,temp,ans,0);
-        return ans;
+       int n=s.size();
+       vector<vector<string>>ans;
+       vector<string>temp;
+       vector<vector<bool>>dp(n,vector<bool>(n,false));
+       for(int i=0;i<n;i++)
+       {
+           dp[i][i]=true;
+       }
+       for(int l=2;l<=n;l++)
+       {
+           for(int i=0;i<n-l+1;i++)
+           {
+               int j=i+l-1;
+               if(s[i]==s[j])
+               {
+                   if(l==2)
+                   {
+                       dp[i][j]=true;
+                   }
+                   else
+                   {
+                       dp[i][j]=dp[i+1][j-1];
+                   }
+               }
+           }
+       }
+       solve(s,temp,ans,0,dp);
+       return ans;
+       
     }
 };
