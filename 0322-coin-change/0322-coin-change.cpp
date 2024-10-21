7b8321478dedae591349684f58bddf9514dbf794
RashmiKuliyal <128361999+Kuliyalrashmi@users.noreply.github.com>
2024-10-21 11:21:22 +0530

    Time: 51 ms (25.18%), Space: 51.3 MB (14.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    int solve(int index,vector<int>&coins,int amount,vector<vector<int>>&dp)
-    {
-        if(amount<=0||index>=coins.size())
-        {
-            if(amount==0)
-            {
-                return 0;
-            }
-            return INT_MAX-1;
-        }
-        if(dp[index][amount]!=-1)
-        {
-            return dp[index][amount];
-        }
-        if(coins[index]>amount)
-        {
-            dp[index][amount]=0+solve(index+1,coins,amount,dp);
-        }
-        else
-        {
-            int include=1+solve(index,coins,amount-coins[index],dp);
-            int exclude=0+solve(index+1,coins,amount,dp);
-            dp[index][amount]=min(include,exclude);
-        }
-        return dp[index][amount];
-    }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        int ans=solve(0,coins,amount,dp);
-        if(ans==INT_MAX-1)
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,INT_MAX-1));
+        for(int i=0;i<=n;i++)
         {
-            return -1;
+            for(int j=0;j<=amount;j++)
+            {
+                if(i==0||j==0)
+                {
+                    dp[i][j]=j==0?0:INT_MAX-1;
+                }
+            }
         }
-        return ans;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                if(coins[i-1]<=j)
+                {
+                    dp[i][j]=min(0+dp[i-1][j],1+dp[i][j-coins[i-1]]);
+                }
+                else
+                {
+                    dp[i][j]=0+dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amount]==INT_MAX-1?-1:dp[n][amount];
     }
 };
