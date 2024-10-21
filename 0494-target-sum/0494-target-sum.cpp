a9a05ec3e7fc0aa3b9fb86c089278dc23ba73db5
RashmiKuliyal <128361999+Kuliyalrashmi@users.noreply.github.com>
2024-10-21 12:18:15 +0530

    Time: 0 ms (100.00%), Space: 14.1 MB (55.69%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,31 +1,5 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&nums,int target,vector<vector<int>>&dp)
-    {
-        if(n==0)
-        {
-            if(target==0&&nums[0]==0)
-            {
-                return 2;
-            }
-            if(target==0||target==nums[0])
-            {
-                return 1;
-            }
-            return 0;
-        }
-        if(dp[n][target]!=-1)
-        {
-            return dp[n][target];
-        }
-        int exclude=solve(n-1,nums,target,dp);
-        int include=0;
-        if(nums[n]<=target)
-        {
-            include=solve(n-1,nums,target-nums[n],dp);
-        }
-        return dp[n][target]=(include+exclude);
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size(),sum=0;
         for(auto num:nums)
@@ -42,7 +16,32 @@ public:
             return 0;
         }
         required/=2;
-        vector<vector<int>>dp(n,vector<int>(required+1,-1));
-        return solve(n-1,nums,required,dp);
+        vector<vector<int>>dp(n,vector<int>(required+1,0));
+        if(nums[0]==0)
+        {
+            dp[0][0]=2;
+        }
+        else
+        {
+            dp[0][0]=1;
+        }
+        if(nums[0]!=0&&nums[0]<=required)
+        {
+            dp[0][nums[0]]=1;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int target=0;target<=required;target++)
+            {
+                int exclude=dp[i-1][target];
+            int include=0;
+            if(nums[i]<=target)
+            {
+                include=dp[i-1][target-nums[i]];
+            }
+            dp[i][target]=include+exclude;
+            }
+        }
+        return dp[n-1][required];
     }
 };
